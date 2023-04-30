b27b192527ae02f54bbc13bedfb9b2cf9e3b19c4
Hilmar Wiegand <me@hwgnd.de>
2023-04-30 19:34:32 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "qmk-dev-shell";
+          nativeBuildInputs = with pkgs; [qmk];
+        };
+      });
+}
